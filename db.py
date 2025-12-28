3bda9e46b8ccbeac7ee540d606ecc57f85acbd4a
abhii981 <abhiilalit@gmail.com>
2025-12-28 23:52:58 +0530

    Remove MySQL completely and use PostgreSQL

--- db.py
+++ db.py
@@ -4,8 +4,9 @@ import streamlit as st
 def get_connection():
     return psycopg2.connect(
         host=st.secrets["PGHOST"],
+        port=st.secrets["PGPORT"],
+        dbname=st.secrets["PGDATABASE"],
         user=st.secrets["PGUSER"],
         password=st.secrets["PGPASSWORD"],
-        dbname=st.secrets["PGDATABASE"],
-        port=st.secrets.get("PGPORT", 5432)
+        sslmode="require"
     )
